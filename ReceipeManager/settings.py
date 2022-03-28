3816c84e5f553d5b1c915ff4ab6757abe919d977
mehmet <>
2022-03-28 21:17:47 +0300

    settings updated..

--- ReceipeManager/settings.py
+++ ReceipeManager/settings.py
@@ -130,9 +130,9 @@ STATIC_ROOT = BASE_DIR / 'static'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOWED_ORIGINS = [
-    "https://dynamic-lokum-0bd285.netlify.app",
-    "https://dynamic-lokum-0bd285.netlify.app/"
-]
+CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOWED_ORIGINS = [
+#     "https://dynamic-lokum-0bd285.netlify.app",
+#     "https://dynamic-lokum-0bd285.netlify.app/"
+# ]
 CORS_ALLOW_METHODS = ['DELETE', 'GET', 'POST', 'PUT']
