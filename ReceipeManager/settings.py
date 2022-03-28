d9efe3ad0569fa8520e78bcfbb5913ff24a94def
mehmet <>
2022-03-28 20:14:57 +0300

    cors updated..

--- ReceipeManager/settings.py
+++ ReceipeManager/settings.py
@@ -57,12 +57,13 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
-# CORS_ALLOWED_ORIGINS = [
-#     "https://example.com",
-#     "https://sub.example.com",
-#     "http://localhost:8080",
-#     "http://127.0.0.1:9000",
-# ]
+CORS_ALLOWED_ORIGINS = [
+    "https://example.com",
+    "https://sub.example.com",
+    "http://localhost:8080",
+    "http://127.0.0.1:9000",
+    "http://localhost"
+]
 
 ROOT_URLCONF = 'ReceipeManager.urls'
 
