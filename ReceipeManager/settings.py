333a7dc19abfb14f3373079e67cdd287bab85322
mehmet <>
2022-03-29 04:10:06 +0300

    settings updated..

--- ReceipeManager/settings.py
+++ ReceipeManager/settings.py
@@ -133,10 +133,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
-    "https://dynamic-lokum-0bd285.netlify.app"
+    "https://dynamic-lokum-0bd285.netlify.app",
+    "http://172.20.10.3"
 ]
 ALLOWED_HOSTS = [
     '0.0.0.0',
     'receipe-manager.herokuapp.com',
-    '127.0.0.1'
+    '127.0.0.1',
+    '172.20.10.3'
 ]
